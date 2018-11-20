efb1a70bae95aeb750b455658190e8e28649eb90
lunana <nala@cgs18.com>
2018-11-20 19:56:56 +0800

    master

--- src/components/staffList.vue
+++ src/components/staffList.vue
@@ -1,8 +1,14 @@
 <template>
  
   <div class="roster">
-    新建  工长  员工  客户 都在这里 
-</div>
+    <dl class="">
+      <dt>D</dt>
+        <dd><span class="name">董春华</span> 13817527029 <span class="arrow-pic"><img  src="../assets/img/Copy2x.png"></span></dd>
+      <dt>L</dt>
+        <dd><span class="name">董春华</span> 13817527029 <span class="arrow-pic"><img  src="../assets/img/Copy2x.png"></span></dd>
+        <dd><span class="name">董春华</span> 13817527029 <span class="arrow-pic"><img  src="../assets/img/Copy2x.png"></span></dd>
+    </dl>
+  </div>
 
 </template>
 
@@ -21,27 +27,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-  .record{
-    background-color: #f6f6f6;
+  .roster dt,.roster dd{
+    border-bottom: solid 1px #C6C6C6; padding-left: 10px;
   }
-  .record h3{
-    padding: 5px 10px;
-    color: #757575;
+  .roster dt{
+      color: #9C9C9C; font-size: 12px; line-height: 16px;  
   }
-  .r-content{
-    padding: 5px 10px;
-    background-color: #fff;
-    color: #A3A4A6;
-    font-size: 16px;
-  }
-  .r-pic{
-    border-top: solid 1px #C6C6C6;
-    padding: 10px 0px 10px 10px;
-    margin-top: 3px;
-  }
-  .record_icon{
-    border: solid 1px #999;
-    width: 75px; 
-    height: 75px;
-  }
+   .roster dd{
+      color: #191F25; font-size: 16px; line-height: 44px; 
+      .name{ display: inline-block; margin-right:20px; }
+      .arrow-pic{ display: inline-block;  float: right; margin-right:10px; }
+      .arrow-pic img{width: 8px; height: 14px;}
+   }
 </style>
