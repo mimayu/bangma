0f875c6b2a0874e87f5ab1c77a0503f2e1490483
alex <961227167@qq.com>
2018-12-05 10:14:40 +0800

    修改

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -3,7 +3,22 @@ module.exports = {
         '/api': {    //将www.exaple.com印射为/apis
             target: 'http://www.51bangma.com',  // 接口域名
             secure: false,  // 如果是https接口，需要配置这个参数
-            changeOrigin: true,  //是否跨域           
+            changeOrigin: true,  //是否跨域  
+            onProxyRes(proxyRes, req, res) {
+                var cookies = proxyRes.headers['set-cookie']
+                if (cookies == null || cookies.length == 0) {
+                    delete proxyRes.headers['set-cookie']
+                    return
+                }
+                for (var i = 0,n = cookies.length; i < n; i++) {
+                    if(cookies[i].match(/^JSESSIONID=[^;]+;[\s\S]*Path=\/[^;]+/)){
+                        cookies[i] = cookies[i].replace(/Path=\/[^;]+/,'Path=/');
+                    }
+                }
+
+                proxyRes.headers['set-cookie'] = cookies;
+            },
+            historyApiFallback: true       
         }
     }
 }
